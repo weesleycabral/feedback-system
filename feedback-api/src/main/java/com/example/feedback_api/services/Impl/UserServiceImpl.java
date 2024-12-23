376451285087a09ee017c964863cb8ba3c6bb981
weesleycabral <weesleylima@hotmail.com>
2024-12-23 15:22:49 -0300

    feat: simplify user retrieval by throwing exception if user not found

--- feedback-api/src/main/java/com/example/feedback_api/services/Impl/UserServiceImpl.java
+++ feedback-api/src/main/java/com/example/feedback_api/services/Impl/UserServiceImpl.java
@@ -2,13 +2,13 @@ package com.example.feedback_api.services.Impl;
 
 import com.example.feedback_api.domain.user.User;
 import com.example.feedback_api.dto.UserDTO;
+import com.example.feedback_api.exceptions.UserNotFoundException;
 import com.example.feedback_api.respositories.UserRepository;
 import com.example.feedback_api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,13 +27,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO getUserById(String id) {
-        Optional<User> user = userRepository.findById(id);
-        if (user.isPresent()) {
-            User foundUser = user.get();
-            return new UserDTO(foundUser.getId(), foundUser.getName(), foundUser.getEmail());
-        } else {
-            return null; // ou lance uma exceção apropriada
-        }
+        User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        return new UserDTO(user.getId(), user.getName(), user.getEmail());
     }
-
 }
